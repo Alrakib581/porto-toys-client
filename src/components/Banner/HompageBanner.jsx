41e809448dcc9eae6a6f5066bce394d7f1ea8399
alrakib581 <alrakib581@gmail.com>
2023-05-18 11:16:21 +0600

    Carousel and banner design added

--- src/components/Banner/HompageBanner.jsx
+++ src/components/Banner/HompageBanner.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import './HomepageBanner.css';
-import bannerImg from '../../../public/banner.jpg';
+import bannerOneImage from '../../../public/bannerOne.jpg'
+import bannerTwoImage from '../../../public/bannerTwo.jpg'
+import { Carousel } from 'react-bootstrap';
 
 const HompageBanner = () => {
     return (
-        <div className='banner'>
-            <h2 className='banner-p fw-bold text-center company-name mt-5 '>Best Online Toy marketplace In  Bangladesh</h2>
-            <p className='text-center fw-semibold company-name'><span className=''>Sell Your sports Toy </span> and earn money without any hasitient.</p>
-            <p className='text-center'> <img className='img-fluid banner-img w-75' src={bannerImg} alt="" /></p>
+        <div>
+            <Carousel fade>
+                <Carousel.Item className='position-relative'>
+                    <img className="d-block w-100"
+                        src={bannerTwoImage}  alt="Banner Image" />
+                    <div className='position-absolute  top-50 start-50 translate-middle text-light text-center'>
+                        <h3 className='fs-1'>Online Toy marketplace In  Bangladesh</h3>
+                        <p>Sell toy and earn money without any hasitient. <span className='text-warning'>seller service Available 24/7</span></p>
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item className='position-relative'>
+                    <img className="d-block w-100"
+                        src={bannerOneImage} alt="Banner Image" />
+                     <div className='position-absolute  top-50 start-50 translate-middle text-light text-center'>
+                        <h3 className='fs-1'>Best seller services We Provide</h3>
+                        <p>Sell toy and earn money without any hasitient. <span className='text-warning'>seller service Available 24/7</span></p>
+                    </div>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
